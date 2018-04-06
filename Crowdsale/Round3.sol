6e12b04420f89170c7a1792b134359cbf75859b2
Carlos <carlos@moonfunding.com>
2018-04-06 13:53:10 +0200

    Update Round3.sol

--- Crowdsale/Round3.sol
+++ Crowdsale/Round3.sol
@@ -48,7 +48,7 @@ contract Crowdsale {
         uint amount = msg.value;
         balanceOf[msg.sender] += amount;
         amountRaised += amount;
-        tokenReward.transfer(msg.sender, amount / price);
+        tokenReward.transfer(msg.sender, amount * price);
         emit FundTransfer(msg.sender, amount, true);
     }
 
